78adc3a80959d4ec9ecbc15710a4c370410b1e10
Maruf Hosen <maruf@thekowcompany.com>
2026-04-16 02:46:35 +0600

    cors add

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,11 @@ import Product from "./models/Product";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 mongoose
